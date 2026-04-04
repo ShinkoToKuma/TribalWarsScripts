28b9f8c67eea6c7a727e1a2ed7aeec6c40ffa3cd
ShinkoToKuma <51796455+ShinkoToKuma@users.noreply.github.com>
2026-04-04 19:40:10 +0200

    Update relicFinder2026.js
    - Store all relics in localstorage
    - Sort by reportid to only show most recent instance found
    - Add map visualisation using mapSDK

--- relicFinder2026.js
+++ relicFinder2026.js
@@ -1,6 +1,275 @@
 //script by Sophie "Shinko to Kuma". discord: Sophie#2418 website: https://www.shinko-to-kuma.com/
 (function () {
     'use strict';
+    /*
+    Map SDK by Thomas "Sass" Ameye
+
+    I am not affiliated to Innogames
+    Under MIT License:
+
+    Copyright 2022 Thomas "Sass" Ameye
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    */
+    function colorWithOpacity(color, opacity) {
+        const temp = document.createElement("canvas").getContext("2d");
+        temp.fillStyle = color;
+        const computed = temp.fillStyle; // normalized color (often rgb)
+        // If already rgb/rgba
+        if (computed.startsWith("rgb")) {
+            const values = computed.match(/\d+/g);
+            return `rgba(${values[0]}, ${values[1]}, ${values[2]}, ${opacity})`;
+        }
+
+        // If hex (#rrggbb or #rgb)
+        if (computed.startsWith("#")) {
+            let hex = computed.slice(1);
+
+            if (hex.length === 3) {
+                hex = hex.split("").map(c => c + c).join("");
+            }
+
+            const num = parseInt(hex, 16);
+            const r = (num >> 16) & 255;
+            const g = (num >> 8) & 255;
+            const b = num & 255;
+
+            return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+        }
+
+        // fallback
+        return computed;
+    }
+    if(game_data.screen === "map") {
+        const MapSdk = {
+            init() {
+                if (this.mapOverlay.mapHandler._spawnSector) {
+                    //exists already, don't recreate
+                } else {
+                    //doesn't exist yet
+                    this.mapOverlay.mapHandler._spawnSector = this.mapOverlay.mapHandler.spawnSector;
+                }
+                this.mapOverlay.mapHandler.spawnSector = (data, sector) => {
+                    this.mapOverlay.mapHandler._spawnSector(data, sector);
+                    // Main map canvas
+                    var beginX = sector.x - data.x;
+                    var endX = beginX + this.mapOverlay.mapSubSectorSize;
+                    var beginY = sector.y - data.y;
+                    var endY = beginY + this.mapOverlay.mapSubSectorSize;
+                    for (var x in data.tiles) {
+                        var x = parseInt(x, 10);
+                        if (x < beginX || x >= endX) {
+                            continue;
+                        }
+                        for (var y in data.tiles[x]) {
+                            var y = parseInt(y, 10);
+                            if (y < beginY || y >= endY) {
+                                continue;
+                            }
+                            // comment is if you don't want to draw anything in sectors where there are no villages to reduce drawing calls.
+                            // var v = this.mapOverlay.villages[(data.x + x) * 1000 + (data.y + y)];
+                            // if (v) {
+                            var el = $('#mapOverlay_canvas_' + sector.x + '_' + sector.y);
+                            if (!el.length) {
+                                var canvas = document.createElement('canvas');
+                                canvas.style.position = 'absolute';
+                                canvas.width = (this.mapOverlay.map.scale[0] * this.mapOverlay.map.sectorSize);
+                                canvas.height = (this.mapOverlay.map.scale[1] * this.mapOverlay.map.sectorSize);
+                                canvas.style.zIndex = 10;
+                                canvas.className = 'mapOverlay_map_canvas';
+                                canvas.id = 'mapOverlay_canvas_' + sector.x + '_' + sector.y;
+                                sector.appendElement(canvas, 0, 0);
+
+                                // draw here
+                                this.redrawSector(sector, canvas);
+                            }
+                            // }
+                        }
+                    }
+
+                    // Topo canvas
+                    for (var key in this.mapOverlay.minimap._loadedSectors) {
+                        var sector = this.mapOverlay.minimap._loadedSectors[key];
+                        var el = $('#mapOverlay_topo_canvas_' + key);
+                        if (!el.length) {
+                            var canvas = document.createElement('canvas');
+                            canvas.style.position = 'absolute';
+                            canvas.width = '250';
+                            canvas.height = '250';
+                            canvas.style.zIndex = 11;
+                            canvas.className = 'mapOverlay_topo_canvas';
+                            canvas.id = 'mapOverlay_topo_canvas_' + key;
+                            sector.appendElement(canvas, 0, 0);
+
+                            this.redrawMiniSector(sector, canvas);
+                        }
+                    }
+
+                }
+                this.mapOverlay.reload();
+                return "Initialised Map SDK";
+            },
+            line(x1, y1, x2, y2, styling, sector, canvas) {
+                let ctx = canvas.getContext('2d');
+                let origin = this.pixelByCoord(sector, x1, y1);
+                let target = this.pixelByCoord(sector, x2, y2);
+                // TODO: only draw in sectors where the line is in
+                ctx.beginPath();
+                if (styling.main && styling.main.strokeStyle) ctx.strokeStyle = styling.main.strokeStyle;
+                if (styling.main && styling.main.lineWidth) ctx.lineWidth = styling.main.lineWidth;
+                if (styling.main && styling.main.fillStyle) ctx.fillStyle = styling.main.fillStyle;
+                ctx.moveTo(origin[0], origin[1]);
+                ctx.lineTo(target[0], target[1]);
+                ctx.stroke();
+                ctx.closePath();
+            },
+            polygon(coords, styling, sector, canvas) {
+                let ctx = canvas.getContext('2d');
+                ctx.save();
+                let origin = this.pixelByCoord(sector, coords[0].x, coords[0].y);
+                if (styling.main && styling.main.strokeStyle) ctx.strokeStyle = styling.main.strokeStyle;
+                if (styling.main && styling.main.lineWidth) ctx.lineWidth = styling.main.lineWidth;
+                if (styling.main && styling.main.fillStyle) ctx.fillStyle = styling.main.fillStyle;
+                ctx.beginPath();
+                ctx.moveTo(origin[0], origin[1]);
+                for (let i = 0; i < coords.length; i++) {
+                    let target = this.pixelByCoord(sector, coords[i].x, coords[i].y);
+                    ctx.lineTo(target[0], target[1]);
+                }
+                ctx.closePath();
+                ctx.stroke();
+                ctx.fill();
+                ctx.restore();
+            },
+            polygonOnMini(coords, styling, sector, canvas) {
+                let ctx = canvas.getContext('2d');
+                ctx.save();
+                let x = (coords[0].x - sector.x) * 5 + 3;
+                let y = (coords[0].y - sector.y) * 5 + 3;
+                if (styling.mini && styling.mini.strokeStyle) ctx.strokeStyle = styling.mini.strokeStyle;
+                if (styling.mini && styling.mini.lineWidth) ctx.lineWidth = styling.mini.lineWidth;
+                if (styling.mini && styling.mini.fillStyle) ctx.fillStyle = styling.mini.fillStyle;
+                ctx.beginPath();
+                ctx.moveTo(x, y);
+                for (let i = 0; i < coords.length; i++) {
+                    x = (coords[i].x - sector.x) * 5 + 3;
+                    y = (coords[i].y - sector.y) * 5 + 3;
+                    ctx.lineTo(x, y);
+                }
+                ctx.closePath();
+                ctx.stroke();
+                ctx.fill();
+                ctx.restore();
+            },
+            iconOnMap(img, x, y, size, sector, canvas) {
+                let ctx = canvas.getContext('2d');
+                let pos = this.pixelByCoord(sector, x, y);
+                // TODO: only draw in sectors where the icon is in
+                ctx.drawImage(img, pos[0] - (size / 2), pos[1] - (size / 2), size, size);
+            },
+            iconOnMiniMap(img, x, y, size, sector, canvas) {
+                let ctx = canvas.getContext('2d');
+                x = (x - sector.x) * 5 + 3;
+                y = (y - sector.y) * 5 + 3;
+                ctx.drawImage(img, x - (size / 2), y - (size / 2), size, size);
+            },
+            pixelByCoord(sector, x, y) {
+                let st_pixel = this.mapOverlay.map.pixelByCoord(sector.x, sector.y);
+                let originXY = this.mapOverlay.map.pixelByCoord(x, y);
+                let originX = (originXY[0] - st_pixel[0]) + this.mapOverlay.tileSize[0] / 2;
+                let originY = (originXY[1] - st_pixel[1]) + this.mapOverlay.tileSize[1] / 2;
+                return [originX, originY];
+            },
+            clearMap() {
+                this.circles = [];
+                this.lines = [];
+                this.texts = [];
+                this.icons = [];
+                // reload the overlay once cleared
+                this.mapOverlay.reload();
+            },
+            redrawSector(sector, canvas) {
+                this.polygons.forEach(element => {
+                    if (element.drawOnMap) this.polygon(element.coords, element.styling, sector, canvas);
+                })
+                this.icons.forEach(element => {
+                    if (element.drawOnMap) this.iconOnMap(element.img && element.img.src != '' ? element.img : this.defaultImg, element.x, element.y, element.mapSize || 20, sector, canvas);
+                })
+            },
+            redrawMiniSector(sector, canvas) {
+                this.icons.forEach(element => {
+                    if (element.drawOnMini) this.iconOnMiniMap(element.img && element.img.src != '' ? element.img : this.defaultImg, element.x, element.y, element.miniSize || 5, sector, canvas);
+                })
+                this.polygons.forEach(element => {
+                    if (element.drawOnMini) this.polygonOnMini(element.coords, element.styling, sector, canvas);
+                })
+            },
+            polygons: [],
+            icons: [],
+            mapOverlay: TWMap,
+            defaultImg: new Image()
+        };
+        MapSdk.defaultImg.src = "/graphic/buildings/wall.png";
+        MapSdk.init();
+        const relics = JSON.parse(localStorage.getItem("shinkoRelicFinderResults")) || [];
+        relics.forEach((relic, index) => {
+            const img = new Image();
+            img.src = relic.img.match('src="([^"]+)"')[1];
+            const x = parseInt(relic.village.substring(0, 3));
+            const y = parseInt(relic.village.substring(4, 7));
+            MapSdk.icons.push({
+                img,
+                x: x-0.29,
+                y: y+0.21,
+                drawOnMini: false,
+                drawOnMap: true,
+                mapSize: 20,
+                miniSize: 5,
+            });
+            MapSdk.polygons.push({
+                coords: [
+                    {
+                        x: x-0.5,
+                        y: y-0.5,
+                    },
+                    {
+                        x: x-0.5,
+                        y: y+0.5,
+                    },
+                    {
+                        x: x+0.5,
+                        y: y+0.5,
+                    },
+                    {
+                        x: x+0.5,
+                        y: y-0.5
+                    }
+                ],
+                styling: {
+                    main: {
+                        "strokeStyle": relic.color,
+                        "lineWidth": 2,
+                        "fillStyle": colorWithOpacity(relic.color, 0.4),
+                    },
+                    mini: {
+                        "strokeStyle": 'black',
+                        "lineWidth": 1,
+                        "fillStyle": colorWithOpacity(relic.color, 0.8),
+                    },
+                },
+                drawOnMini: true,
+                drawOnMap: true,
+            });
+        });
+
+        setTimeout(()=>MapSdk.mapOverlay.reload(), 500);
+        return;
+    }
     const LANGUAGES = {
         en_DK: {
             noReportsFound: "No reports found! Please run this script from the report list or loot assistant list report list",
@@ -262,16 +531,18 @@
             const uniqueRelics = [];
             const seenVillages = new Set();
 
+            // sort by report ID to make sure we take the most recent information
+            this.relics.sort((a, b) => parseInt(b.reportId) - parseInt(a.reportId));
             this.relics.forEach(relic => {
                 if (!seenVillages.has(relic.village)) {
                     seenVillages.add(relic.village);
                     uniqueRelics.push(relic);
                 }
             });
+            localStorage.setItem("shinkoRelicFinderResults", JSON.stringify(this.relics));
 
             // sort by distance
             uniqueRelics.sort((a, b) => a.distance - b.distance);
-            localStorage.setItem("shinkoRelicFinderResults", JSON.stringify(uniqueRelics));
 
             const renownedCount = uniqueRelics.filter(r => r.quality === LANG.qualities.renowned).length;
             const superiorCount = uniqueRelics.filter(r => r.quality === LANG.qualities.superior).length;
@@ -349,7 +620,7 @@
             const dx = Math.abs(x - game_data.village.x);
             const dy = Math.abs(y - game_data.village.y);
             return dx + dy;
-        }
+        },
     };
 
 
